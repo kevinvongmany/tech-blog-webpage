57ebf4d2d2bd607cb334c73c2f5c64121d0aef71
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-09-08 16:51:44 +1000

    Updated a way to update an existing post in the application

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -76,6 +76,46 @@ router.get('/dashboard', withAuth, async (req, res) => {
   }
 });
 
+
+router.get('/addpost', withAuth, (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+  res.render('addpost', {
+    logged_in: true
+  });
+});
+
+
+router.get('/updatepost/:id', withAuth, async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('updatepost', {
+      ...post,
+      logged_in: true
+    }); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -96,15 +136,6 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
-router.get('/addpost', withAuth, (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (!req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-  res.render('addpost', {
-    logged_in: true
-  });
-});
+
 
 module.exports = router;
